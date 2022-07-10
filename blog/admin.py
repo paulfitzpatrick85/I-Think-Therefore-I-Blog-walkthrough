d17465285869f9f095d83c4318cb5c4043338c93
paulfitzpatrick85 <fitzer99@gmail.com>
2022-07-10 21:07:59 +0000

    build admin sit p2/posts,comments,approve action

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
-from .models import Post 
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}  # populate slug(form?) from title
+    list_filter = ('status', 'created_on')  # will create filter window/box with status and created today, last 7 days etc
+    list_display = ('title', 'slug', 'status', 'created_on')  # titles/info on each post
+    search_fields = ('title', 'content')  # add search bar in admin page
+
     summernote_fields = ('content')
 
 
-# Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    action = ['approve_comments']      #approve comment box in admin, actions can take list of functions
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)    #approve is boolean which is false by default, this updates quearyset to true
