8856b6cf67b3e4343e492fc46df83c08a6afd50b
paulfitzpatrick85 <fitzer99@gmail.com>
2022-07-10 22:15:00 +0000

    view creation checklist

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')  # contents of post table
+    template_name = 'index.html' # html file that view will render
+    paginate_by = 6  # limit number of post to 6, if there are more django will add page navagation
 
 # Create your views here.
